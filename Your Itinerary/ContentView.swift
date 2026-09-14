14d95c29d74b27d75af968d257d77d999eb5ef91
Romelyn Argoncillo <romelyn.argoncillo2213@gmail.com>
2026-09-14 15:16:57 +0100

    Initial Commit

---
+++ Your Itinerary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Your Itinerary
+//
+//  Created by Lyn A on 14/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Your Itinerary/Your_ItineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_ItineraryApp.swift
+//  Your Itinerary
+//
+//  Created by Lyn A on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Your_ItineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
